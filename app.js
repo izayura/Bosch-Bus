3249cb9bfde59d7b5384e67fa44ed3d20d7a0d6e
Nienorloth <nienorloth@gmail.com>
2019-12-05 22:34:51 -0600

    data visualizada

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyA7njoATlPyzGF60LsDfujCJZ1pKJiFjnQ",
-    authDomain: "bosch-bus.firebaseapp.com",
-    databaseURL: "https://bosch-bus.firebaseio.com",
-    projectId: "bosch-bus",
-    messagingSenderId: "222527960687",
-    appId: "1:222527960687:web:cd36b5877a7c224a707f9a",
-  };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);const app = firebase.app();
-let usersArray = [];
-const showButton = document.getElementById("showButton");
-const cardsContainer = document.getElementById("card");
-const buttonData = document.getElementById('showData');
 const ctx = document.getElementById('myChart').getContext('2d');
 const chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -41,19 +27,5 @@ const chart = new Chart(ctx, {
     options: {}
 });
 
-dbRef.on("child_added", snap => {
-    const li = document.createElement("li");
-    li.innerText = snap.val();
-    cardsContainer.appendChild(li);
-});
-
-// const showData = () => {
-//     usersArray = snap.val();
-//     usersArray.map(user => {
-//         cardsContainer.innerHTML+=
-//     `<h3>${user.Name}</h3>`
-//     });
-
-// showButton.addEventListener("click", showData);
 
 
