8cca45c2324083419254bb39712538b1e36e4b81
lessly <treessleess@gmail.com>
2019-12-05 23:46:27 -0600

    se agrega block y none funcional a map y grapics

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-let button_block = document.getElementById('mapGraphic');
+// let button_block = document.getElementById(block);
+document.getElementById('mapGraphic2').style.display = 'none';
+        document.getElementById('myChart').style.display = 'block';
+        document.getElementById('map').style.display = 'none';
+        document.getElementById('mapGraphic').style.display = 'block';
 
-button_block.addEventListener('click', () => {
-   
-        document.getElementById('myChart').style.display = 'none';
-        document.getElementById('map').style.display = 'block';
-      
-    
-});
-let button_block2 = document.getElementById('mapGraphic');
+
+
+
+let button_block2 = document.getElementById('mapGraphic2');
+
 
 button_block2.addEventListener('click', () => {
-   
-        document.getElementById('myChart').style.display = 'none';
-        // document.getElementById('mapGraphic').style.display = 'none';
-      
-    
+        console.log(document.getElementById('myChart'))
+        document.getElementById('mapGraphic2').style.display = 'none';
+        document.getElementById('myChart').style.display = 'block';
+        document.getElementById('map').style.display = 'none';
+        document.getElementById('mapGraphic').style.display = 'block';
+
+
 });
 
 
+let button_block = document.getElementById('mapGraphic');
 
 
+button_block.addEventListener('click', () => {
+        console.log(document.getElementById('myChart'))
+        document.getElementById('myChart').style.display = 'none';
+        document.getElementById('map').style.display = 'block';
+        document.getElementById('mapGraphic').style.display = 'none';
+        document.getElementById('mapGraphic2').style.display = 'block';
+
+
+});
