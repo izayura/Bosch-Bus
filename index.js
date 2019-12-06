3d07c6bc103244e3a84292e2ab8e1ef6d1416497
lessly <treessleess@gmail.com>
2019-12-05 20:24:53 -0600

    se agrega mapa y graficas -se elimina selector -se alinean mapa y graficas a la derecha -se inicia archivo index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// let button_block = document.getElementById(block);
+
+document.addEventListener('click', () => {
+    function block(){
+        document.getElementById('mapGraphic').style.display = 'none';
+        document.getElementById('mapGraphic').style.display = 'block';
+        }
+    
+});
+
+
+
+
