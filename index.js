2704bff4cb861e70008df8823b2d24a1fe6c9d93
lessly <treessleess@gmail.com>
2019-12-05 21:37:42 -0600

    se agrega nuevo orden en html y se agrega funcion de ocultar

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// let button_block = document.getElementById(block);
+let button_block = document.getElementById('mapGraphic');
 
-document.addEventListener('click', () => {
-    function block(){
-        document.getElementById('mapGraphic').style.display = 'none';
-        document.getElementById('mapGraphic').style.display = 'block';
-        }
+button_block.addEventListener('click', () => {
+   
+        document.getElementById('myChart').style.display = 'none';
+        document.getElementById('map').style.display = 'block';
+      
+    
+});
+let button_block2 = document.getElementById('mapGraphic');
+
+button_block2.addEventListener('click', () => {
+   
+        document.getElementById('myChart').style.display = 'none';
+        // document.getElementById('mapGraphic').style.display = 'none';
+      
     
 });
 
